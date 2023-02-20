062e724030000f725a91567d1bd517cc36865f0f
Tristan Havelick <tristan@havelick.com>
2023-02-19 22:18:04 -0700

    Add minutes to read each article

--- brutal_bag/models/article.py
+++ brutal_bag/models/article.py
@@ -99,3 +99,17 @@ class Article:
             return ""
 
         return humanize.naturaltime(datetime.utcnow() - self.date)
+
+    def minutes_read(self):
+        "return the number of minutes to read the article"
+
+        # reading time contains the number of minutes it will take to read
+        # the article. This function will return a string like "5 minutes read"
+        # or "1 minute read".
+        if not self.reading_time:
+            return ""
+
+        if self.reading_time == 1:
+            return "1 minute read"
+
+        return f"{self.reading_time} minutes read"
