5d2603f7f0930e9bd6d0959179a5b1db74c6def5
Tristan Havelick <tristan@havelick.com>
2023-02-20 18:55:46 -0700

    Add empty tag model, remove dead code from article

--- brutal_bag/models/article.py
+++ brutal_bag/models/article.py
@@ -5,9 +5,6 @@ from datetime import datetime, timedelta
 import humanize
 
 
-from .wallabag import Wallabag
-
-
 @dataclass
 class Article:
     "An article from a reader or bookmarking service like Wallabag"
@@ -20,41 +17,6 @@ class Article:
     date: datetime = None
     reading_time: int = None
     tags: list = None
-
-    @staticmethod
-    def from_wallabag_dict(source_dict):
-        "Create an Article from a wallabag response dict."
-        dest_dict = {
-            key: source_dict.get(key)
-            for key in [
-                "content",
-                "published_by",
-                "reading_time",
-                "title",
-            ]
-        }
-
-        dest_dict["id"] = str(source_dict.get("id"))
-        dest_dict["external_url"] = source_dict.get("url")
-        dest_dict["tags"] = source_dict.get("tags", [])
-
-        # should probably refactor all this at some point to save
-        # both published_at and created_at in the Article model,
-        # then just render the right one in the template
-        article_date = source_dict.get("published_at") or source_dict.get("created_at")
-
-        if article_date:
-            # article_date looks like 2019-11-13T13:51:47-0700
-            # let's parse that to a datetime without a time zone
-            time_zone_part = article_date[-5:]
-            date = datetime.strptime(article_date[:-5], "%Y-%m-%dT%H:%M:%S")
-            date = date - timedelta(
-                hours=int(time_zone_part[:3]), minutes=int(time_zone_part[3:])
-            )
-
-            dest_dict["date"] = date
-
-        return Article(**dest_dict)
 
     def published_by_str(self):
         "return a string of the publishers"
